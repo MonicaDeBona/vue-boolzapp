948fc0bc17af114418589cadea6bc9be344e7e0b
MonicaDeBona <debona.monica@outlook.it>
2022-12-16 16:16:51 +0100

    Trying automatic reply

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@
 Milestone 1
     Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
     Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+Milestone 2
+    Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+    Click sul contatto mostra la conversazione del contatto cliccato
+
+Milestone 3
+    Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+    Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 */
 
 const {createApp} = Vue;
@@ -11,6 +19,7 @@ createApp({
         return{
             currentContact: 0,
             newMsg : '',
+            newReply: 'ok',
             contacts: [
                 {
                     name: 'Michele',
@@ -190,11 +199,12 @@ createApp({
                     message: this.newMsg,
                     status: 'sent'
                 });
+                this.contacts[this.currentContact].messages.push ({
+                    date: '16/12/2022 11:50:00',
+                    message: this.newReply,
+                    status: 'received'
+                });
             }
-            
-            this.newMsg = '';
         },
-
-
     }
 }).mount("#app")
